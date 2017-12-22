adbf16fed392b6476de5bd20b49972d367b0184e
Алексей Белов <Alenorze@gmail.com>
2017-12-22 19:26:11 +0200

    Static Files

--- src/microtw/settings.py
+++ src/microtw/settings.py
@@ -121,4 +121,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = posixpath.join(*(BASE_DIR.split(os.path.sep) + ['static']))
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static-storage"),
+]
+
+ 
+STATIC_ROOT =  os.path.join(os.path.dirname(BASE_DIR), "static-serve") 
