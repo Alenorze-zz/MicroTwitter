cadf703bb196f685333483008a54165c2bc9cea0
Алексей Белов <Alenorze@gmail.com>
2017-12-23 01:50:19 +0200

    Fix

--- src/tweets/models.py
+++ src/tweets/models.py
@@ -1,15 +1,15 @@
 from django.conf import settings
 from django.core.exceptions import ValidationError
 from django.db import models
- 
+
 from .validators import validate_content
 
 
 class Tweet(models.Model):
-    user      = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    content   = models.CharField(max_length=256, validators=[validate_content])
-    updated   = models.DateTimeField(auto_now=True)
-    timestamp = models.DateTimeField(auto_now_add=True)
+    user        = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    content     = models.CharField(max_length=140, validators=[validate_content])
+    updated     = models.DateTimeField(auto_now=True)
+    timestamp   = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return str(self.content)
--- src/tweets/views.py
+++ src/tweets/views.py
@@ -1,28 +1,55 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.shortcuts import render
-from django.views.generic import CreateView, ListView, DetailView
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse_lazy
+from django.views.generic import (
+                CreateView,
+                DetailView,
+                DeleteView, 
+                ListView, 
+                UpdateView
+                )
 
-from .models import Tweet
-from .mixins import FormUserNeededMixin, UserOwnerMixin
 from .forms import TweetModelForm
-
+from .mixins import FormUserNeededMixin, UserOwnerMixin
+from .models import Tweet
 
 class TweetCreateView(FormUserNeededMixin, CreateView):
     form_class = TweetModelForm
     template_name = 'tweets/create_view.html'
-    success_url = '/tweets/create/'
-    #login_url = '/admin/'
-
-class TweetListView(DetailView):
-    queryset = Tweet.objects.all()
-
-
-class TweetDetailView(DetailView):
-    queryset = Tweet.objects.all()
+    success_url = "/tweet/create/"
 
 
 class TweetUpdateView(LoginRequiredMixin, UserOwnerMixin, UpdateView):
     queryset = Tweet.objects.all()
     form_class = TweetModelForm
     template_name = 'tweets/update_view.html'
-    success_url = '/tweet/'
+    success_url = "/tweet/"
+
+
+class TweetDeleteView(LoginRequiredMixin, DeleteView):
+    model = Tweet
+    template_name = 'tweets/delete_confirm.html'
+    success_url = reverse_lazy("home")
+
+
+
+class TweetDetailView(DetailView):
+    queryset = Tweet.objects.all()
+
+
+class TweetListView(ListView):
+    queryset = Tweet.objects.all()
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(TweetListView, self).get_context_data(*args, **kwargs)
+        return context
+
+
+def tweet_detail_view(request, pk=None): # pk == id
+    #obj = Tweet.objects.get(pk=pk) # GET from database
+    obj = get_object_or_404(Tweet, pk=pk)
+    print(obj)
+    context = {
+        "object": obj
+    }
+    return render(request, "tweets/detail_view.html", context)
