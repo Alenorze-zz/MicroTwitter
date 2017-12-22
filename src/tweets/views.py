6b86807f35db78cfdd887e5cbadd3400f8ed411e
Алексей Белов <Alenorze@gmail.com>
2017-12-23 01:09:34 +0200

    Login Required Mixins

--- src/tweets/views.py
+++ src/tweets/views.py
@@ -1,18 +1,17 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
 from django.views.generic import CreateView, ListView, DetailView
 
 from .models import Tweet
+from .mixins import FormUserNeededMixin
 from .forms import TweetModelForm
 
-class TweetCreateView(CreateView):
+
+class TweetCreateView(FormUserNeededMixin, CreateView):
     form_class = TweetModelForm
     template_name = 'tweets/create_view.html'
     success_url = '/tweets/create/'
-
-    def form_valid(self, form):
-        form.instance.user = self.request.user
-        return super(TweetCreateView, self).form_valid(form)
-
+    #login_url = '/admin/'
 
 class TweetListView(DetailView):
     queryset = Tweet.objects.all()
