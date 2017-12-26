48537a396ef1036370f6cfb3c0ae82cd1326f8ae
Алексей Белов <Alenorze@gmail.com>
2017-12-26 02:24:48 +0200

    Tweet from Homepage

--- src/tweets/urls.py
+++ src/tweets/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url
-
+from django.views.generic.base import RedirectView
 
 from .views import (
     TweetCreateView,
@@ -11,11 +11,12 @@ from .views import (
 
 app_name = 'tweets'
 
+
 urlpatterns = [
-    url(r'^$', TweetListView.as_view(), name='list'), # /tweet/
+    url(r'^$', RedirectView.as_view(url="/")),
+    url(r'^search/$', TweetListView.as_view(), name='list'),
     url(r'^create/$', TweetCreateView.as_view(), name='create'), # /tweet/create/
     url(r'^(?P<pk>\d+)/$', TweetDetailView.as_view(), name='detail'), # /tweet/1/
     url(r'^(?P<pk>\d+)/update/$', TweetUpdateView.as_view(), name='update'), # /tweet/1/update/
     url(r'^(?P<pk>\d+)/delete/$', TweetDeleteView.as_view(), name='delete'), # /tweet/1/delete/
 ]
-
