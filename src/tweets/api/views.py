f76f7f11b6c04ee645cd0ed14b6bb7b5cca154ec
Алексей Белов <Alenorze@gmail.com>
2017-12-31 02:01:44 +0200

    Display Following Tweets

--- src/tweets/api/views.py
+++ src/tweets/api/views.py
@@ -22,6 +22,22 @@ class TweetListAPIView(generics.ListAPIView):
     pagination_class = StandartResultsPagination
 
     def get_queryset(self, *args, **kwargs):
+        im_following = self.request.user.profile.get_following()
+        qs = Tweet.objects.filter(user__in=im_following).order_by("-timestamp")
+        query = self.request.GET.get("q", None)
+        if query is not None:
+            qs = qs.filter(
+                Q(content__icontains=query)|
+                Q(user__username__icontains=query)
+                )
+        return qs
+
+
+class TweetListAPIView(generics.ListAPIView):
+    serializer_class = TweetModelSerializer
+    pagination_class = StandartResultsPagination
+
+    def get_queryset(self, *args, **kwargs):
         qs = Tweet.objects.all().order_by("-timestamp")
         query = self.request.GET.get("q", None)
         if query is not None:
