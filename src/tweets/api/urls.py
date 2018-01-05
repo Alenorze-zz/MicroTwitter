187f3a65c5f5b1b1cd7516304201df78c31789fd
Алексей Белов <Alenorze@gmail.com>
2018-01-05 10:11:37 +0200

    Like Tweet

--- src/tweets/api/urls.py
+++ src/tweets/api/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import url
 from django.views.generic.base import RedirectView
 
 from .views import (
+    LikeToggleAPIView,
     TweetCreateAPIView,
     TweetListAPIView,
     RetweetAPIView,
@@ -13,6 +14,7 @@ app_name = 'tweet-api'
 
 urlpatterns = [
     url(r'^$', TweetListAPIView.as_view(), name='list'),
+    url(r'^(?P<pk>\d+)/like/$', LikeToggleAPIView.as_view(), name='like-toggle'),
     url(r'^(?P<pk>\d+)/retweet/$', RetweetAPIView.as_view(), name='retweet'),
     url(r'^create/$', TweetCreateAPIView.as_view(), name='create'),
 ]
--- src/tweets/api/views.py
+++ src/tweets/api/views.py
@@ -10,6 +10,17 @@ from .serializers import TweetModelSerializer
 from .pagination import StandartResultsPagination
 
 
+class LikeToggleAPIView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+    def get(self, request, pk, format=None):
+        tweet_qs = Tweet.objects.filter(pk=pk)
+        message = "Not allowed"
+        if request.user.is_authenticated():
+            is_liked = Tweet.objects.like_toggle(request.user, tweet_qs.first())
+            return Response({'liked': is_liked})
+        return Response({"message": message}, status=400)
+
+
 class RetweetAPIView(APIView):
     permission_classes = [permissions.IsAuthenticated]
     def get(self, request, pk, format=None):
