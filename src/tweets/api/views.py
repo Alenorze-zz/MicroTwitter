1c9031e5db7d16aa210b2a75a389de9defb3d932
Алексей Белов <Alenorze@gmail.com>
2018-01-06 12:24:08 +0200

    Search API View

--- src/tweets/api/views.py
+++ src/tweets/api/views.py
@@ -87,6 +87,22 @@ class TweetListAPIView(generics.ListAPIView):
         return qs
 
 
+class SearchTweetAPIView(generics.ListAPIView):
+    queryset = Tweet.objects.all().order_by("-timestamp")
+    serializer_class = TweetModelSerializer
+    pagination_class = StandartResultsPagination
+
+    def get_serializer_context(self, *args, **kwargs):
+        qs = self.queryset
+        query = self.request.GET.get("q", None)
+        if query is not None:
+            qs =  qs.filter(
+                    Q(content__icontains=query) |
+                    Q(user__username__icontains=query)
+                    )
+        return qs 
+
+
 class TweetListAPIView(generics.ListAPIView):
     serializer_class = TweetModelSerializer
     pagination_class = StandartResultsPagination
