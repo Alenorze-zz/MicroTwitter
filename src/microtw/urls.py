e2c39fd95454f680c9758e349dc08b07f517045f
Алексей Белов <Alenorze@gmail.com>
2018-01-06 12:39:56 +0200

    HashTag Tweet List

--- src/microtw/urls.py
+++ src/microtw/urls.py
@@ -3,8 +3,11 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 
+from hashtags.api.views import TagTweetAPIView
 from hashtags.views import HashTagView
 from tweets.views import TweetListView
+from tweets.api.views import SearchTweetAPIView
+from .views import home, SearchView
 
 
 urlpatterns = [
@@ -12,6 +15,7 @@ urlpatterns = [
     url(r'^$', TweetListView.as_view(), name='home'),
     url(r'^tags/(?P<hashtag>.*)/$', HashTagView.as_view(), name='hashtag'),
     url(r'^tweet/', include('tweets.urls', namespace='tweet')),
+    url(r'^api/tags/(?P<hashtag>.*)/$', TagTweetAPIView.as_view(), name='tag-tweet-api'), 
     url(r'^api/tweet/', include('tweets.api.urls', namespace='tweet-api')),
     url(r'^api/', include('accounts.api.urls', namespace='profiles-api')),
     url(r'^', include('accounts.urls', namespace='profiles'))
